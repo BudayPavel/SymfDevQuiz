412c463d990265f7fd4319d8e32f4310b30ea3f6
Unknown <2345pawel@mail.ru>
2018-02-13 11:24:41 +0300

    Admin template
    Add admin template

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Павел
+ * Date: 13.02.2018
+ * Time: 10:56
+ */
+declare(strict_types=1);
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/administrator")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     */
+    public function admin()
+    {
+        return new Response($this->render('admin/admin.html.twig'));
+    }
+}
